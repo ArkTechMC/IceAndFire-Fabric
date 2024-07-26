e0d34e73a2b6510d5deaa8a1ffeb7c3d44a4ce0c
IAFEnvoy <tanksheng545@126.com>
2024-07-26 10:06:56 +0800

    maybe fix a dragon rider bug?

--- src/main/java/com/iafenvoy/iceandfire/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/com/iafenvoy/iceandfire/mixin/PlayerEntityRendererMixin.java
@@ -4,6 +4,7 @@ import com.iafenvoy.iceandfire.entity.EntityDragonBase;
 import com.iafenvoy.iceandfire.render.entity.layer.LayerDragonRider;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -16,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class PlayerEntityRendererMixin {
     @Inject(method = "render(Lnet/minecraft/client/network/AbstractClientPlayerEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"), cancellable = true)
     private void onPlayerRender(AbstractClientPlayerEntity abstractClientPlayerEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        if (abstractClientPlayerEntity.getVehicle() instanceof EntityDragonBase && (MinecraftClient.getInstance().options.getPerspective().isFirstPerson() || !LayerDragonRider.renderingRiders.contains(abstractClientPlayerEntity)))
+        if (abstractClientPlayerEntity.getVehicle() instanceof EntityDragonBase && abstractClientPlayerEntity instanceof ClientPlayerEntity && (MinecraftClient.getInstance().options.getPerspective().isFirstPerson() || !LayerDragonRider.renderingRiders.contains(abstractClientPlayerEntity)))
             ci.cancel();
     }
 }
